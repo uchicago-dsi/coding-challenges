ec12490a34a79cbbf10c3a9a94b5b221111721e6
David Jacobson <dhjacobson@Davids-MacBook-Pro.local>
2025-03-10 14:23:53 -0500

    Clean up to pass ruff.

---
+++ challenges/water_bottle_takehome/candidate_code/water_bottle_challenge.py
@@ -0,0 +1,5 @@
+"""Skeleton for expected solution from candidates."""
+
+def classify_preprocessed_audio(fpath: str) -> int:
+    """Classify preprocessed audio data as top (0) or bottom (1)."""
+    return 0
