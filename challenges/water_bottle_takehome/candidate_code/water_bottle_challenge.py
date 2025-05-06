2ada6ba469b2f13099b43307aedd7a9a6d415824
David Jacobson <dhjacobson@Davids-MacBook-Pro.local>
2025-05-06 17:07:08 -0500

    Add code for IH interview.

--- challenges/water_bottle_takehome/candidate_code/water_bottle_challenge.py
+++ challenges/water_bottle_takehome/candidate_code/water_bottle_challenge.py
@@ -1,5 +1,28 @@
-"""Skeleton for expected solution from candidates."""
+import pandas as pd
+
+# Hard Coded for now. Refactor later.
+THRESHOLD = 2018.73779296875
+MARGIN = 381.1376953125
 
 def classify_preprocessed_audio(fpath: str) -> int:
-    """Classify preprocessed audio data as top (0) or bottom (1)."""
-    return 0
+    """
+    Returns:
+      0: Top strike
+      1: Bottom strike
+      None: Uncertain
+    """
+    try:
+        df = pd.read_csv(fpath, index_col=0).astype(float)
+    except Exception as e:
+        print(f"Error loading '{fpath}': {e}")
+        return None
+
+    # Energy at different Hz 
+    mean_freq = df.mean(axis=1)
+    peak_hz   = mean_freq.idxmax()
+    print(peak_hz)
+
+    if abs(peak_hz - THRESHOLD) < MARGIN:
+        return None
+    
+    return 0 if peak_hz > THRESHOLD else 1
